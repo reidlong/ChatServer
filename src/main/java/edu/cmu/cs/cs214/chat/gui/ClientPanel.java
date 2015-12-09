5563a1ecdf566b7e3625acb9f4ac3a973851ab65
Tianyu Li <tli2@andrew.cmu.edu>
2015-12-09 11:07:21 -0500

    added timestamp

--- src/main/java/edu/cmu/cs/cs214/chat/gui/ClientPanel.java
+++ src/main/java/edu/cmu/cs/cs214/chat/gui/ClientPanel.java
@@ -207,7 +207,7 @@ public class ClientPanel extends JPanel implements ClientChangeListener {
         // TODO: Make the server show the timestamp of the received message.
         // Example output: [15:21:40 -0400] Person: Some message...
 
-        String newText = String.format(" %s: %s%n", msg.getSender(),
+        String newText = String.format("%s %s: %s%n", dateFormatter.toString(), msg.getSender(),
                 msg.getContent());
         this.chatArea.append(newText);
         chatArea.setCaretPosition(chatArea.getDocument().getLength());
